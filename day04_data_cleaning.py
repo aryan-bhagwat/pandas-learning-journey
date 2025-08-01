31e18e2e6250e2b2db5ee41ff8ac92c4076c519d
aryan-bhagwat <aryanbhagwat90@gmail.com>
2025-08-01 18:37:19 +0530

    Added Data

---
+++ day04_data_cleaning.py
@@ -0,0 +1,10 @@
+# Day 4 : Data Cleaning: Missing Data, Duplicates
+
+import pandas as pd
+import numpy as np
+
+# 1. Load the dataset
+
+df = pd.read_csv('C:\\Aryan\\GitHub Repo\\pandas-learning-journey\\dirty_data.csv')
+print(f"\nInitial DataFrame:\n{df}")
+

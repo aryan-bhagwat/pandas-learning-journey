3b89a656870072ee2625207c1286e8fcdd72dace
aryan-bhagwat <aryanbhagwat90@gmail.com>
2025-08-10 20:31:08 +0530

    Day 8 : Time Series in Pandas

--- day08_time_series.py
+++ day08_time_series.py
@@ -1,42 +1,42 @@
-# # Day 8 – Time Series in Pandas
+# Day 8 : Time Series in Pandas
 
 import pandas as pd
 
-# # Sample data
+# Sample data
 
-# data = {
-#     'Date': ['2025-02-08', '2025-07-30', '2025-07-26', '2025-10-19', '2025-12-15'],
-#     'Sales': [200, 220, 250, 275, 300],
-# }
+data = {
+    'Date': ['2025-02-08', '2025-07-30', '2025-07-26', '2025-10-19', '2025-12-15'],
+    'Sales': [200, 220, 250, 275, 300],
+}
 
-# df = pd.DataFrame(data)
+df = pd.DataFrame(data)
 
-# # 1. Covert Date Column to Datetime
-# df['Date'] = pd.to_datetime(df['Date'])
-# print(f"DataFrame with Date Column as Datetime:\n{df}\n")
+# 1. Covert Date Column to Datetime
+df['Date'] = pd.to_datetime(df['Date'])
+print(f"DataFrame with Date Column as Datetime:\n{df}\n")
 
-# # 2. Extract Year, Month, Day, Weekday
-# df['Year'] = df['Date'].dt.year
-# df['Month'] =df['Date'].dt.month
-# df['Day'] = df['Date'].dt.day
-# df['Weekday'] = df['Date'].dt.day_name()
-# print(f"DataFrame with Year, Month, Day, and Weekday:\n{df}\n")
+# 2. Extract Year, Month, Day, Weekday
+df['Year'] = df['Date'].dt.year
+df['Month'] =df['Date'].dt.month
+df['Day'] = df['Date'].dt.day
+df['Weekday'] = df['Date'].dt.day_name()
+print(f"DataFrame with Year, Month, Day, and Weekday:\n{df}\n")
 
-# # 3. Set Date as Index
-# df.set_index('Date', inplace = True)
-# print(f"DataFrame with Date as Index:\n{df}\n")
+# 3. Set Date as Index
+df.set_index('Date', inplace = True)
+print(f"DataFrame with Date as Index:\n{df}\n")
 
-# # 4. Filter data for February 2025
-# feb_data = df.loc["2025-02"]
-# print(f"Data for February 2025:\n{feb_data}\n")
+# 4. Filter data for February 2025
+feb_data = df.loc["2025-02"]
+print(f"Data for February 2025:\n{feb_data}\n")
 
-# # 5. Resample Montly and get total Sales
-# montly_sales = df['Sales'].resample('M').sum()
-# print(f"Monthly Sales:\n{montly_sales}\n")
+# 5. Resample Montly and get total Sales
+montly_sales = df['Sales'].resample('M').sum()
+print(f"Monthly Sales:\n{montly_sales}\n")
 
-# print(f"Full Details:\n{df}\n")
-# print(f"February Sales Details:\n{feb_data}\n")
-# print(f"Monthly Sales Details:\n{montly_sales}\n")
+print(f"Full Details:\n{df}\n")
+print(f"February Sales Details:\n{feb_data}\n")
+print(f"Monthly Sales Details:\n{montly_sales}\n")
 
 
 ##  Practice Tasks
